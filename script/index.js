46f17d478c33a615390e37a365d5347407773af0
Abdallah-Katbai <abdallah.katbai@gmail.com>
2023-11-12 22:46:28 +0200

    adjusted the check functionality and and added the edit functionality

--- script/index.js
+++ script/index.js
@@ -10,7 +10,7 @@ add_bt.addEventListener('click', function(){
     if(txt_bar.value != ''){
     const new_task = document.createElement('div')
     new_task.classList.add('task')   
-    new_task.innerHTML = '<button class="checkbox"><img src="assets and mages/91-918618_checked-checkbox-512-yellow-check-icon-orange-png.png" alt=""></button><div class="set-text">' + txt_bar.value + '</div>'
+    new_task.innerHTML = '<div class = "check-del"><button class="checkbox"><img src="assets and mages/91-918618_checked-checkbox-512-yellow-check-icon-orange-png.png" alt=""></button><button class="del"><p>Delete</p></button></div><div class="set-text">' + txt_bar.value + '</div>'
     tasks.insertBefore(new_task, tasks.firstChild)
     txt_bar.value = ''
     counter += 1
@@ -32,19 +32,33 @@ tasks.addEventListener('click', function (event) {
         // Now you can manipulate the corresponding set-text element
         const txt = document.querySelectorAll('.set-text')[index];
         txt.classList.toggle('crossed');
+        if(checkbox.innerHTML == '<img src="assets and mages/91-918618_checked-checkbox-512-yellow-check-icon-orange-png.png" alt="">'){
+        checkbox.innerHTML = '<img src="assets and mages/91-918618_checked-checkbox-512-yellow-check-icon-orange-png (1).png" alt="">'
+        }
+        else if (checkbox.innerHTML == '<img src="assets and mages/91-918618_checked-checkbox-512-yellow-check-icon-orange-png (1).png" alt="">'){
+            checkbox.innerHTML = '<img src="assets and mages/91-918618_checked-checkbox-512-yellow-check-icon-orange-png.png" alt="">'
+        }
     }
 });
 
-txt.forEach((elm, index) => {
-    elm.addEventListener('dblclick', function(){
-        let temp = txt[index].innertext
-        txt[index].innerHTML = '<input name="edit-task" id="edit-task" placeholder="" />'
+tasks.addEventListener('dblclick', function (event) {
+    const target = event.target
+    const settxt = target.closest('.set-text');
+
+    if (settxt) {
+        const index = Array.from(target.closest('.task').parentNode.children).indexOf(target.closest('.task'))
+        console.log('text choosed at index: ', index)
+        // Now you can manipulate the corresponding set-text element
+        const text = document.querySelectorAll('.set-text')[index];
+        //text.classList.toggle('crossed');
+        let temp = text.innerText
+        text.innerHTML = '<input name="edit-task" id="edit-task" placeholder="" />'
         const edit_txt = document.getElementById('edit-task')
         edit_txt.value = temp
         edit_txt.addEventListener('keypress', function(event){
             if (event.key === "Enter"){
-                txt[index].innerHTML = temp
+                text.innerHTML = edit_txt.value
             }
         })
-    })
+    }
 })
