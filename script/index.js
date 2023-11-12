3ca96559f35cc600391b8a5d08a56fecce77fc42
Abdallah-Katbai <abdallah.katbai@gmail.com>
2023-11-12 23:21:15 +0200

    added the delete functionality

--- script/index.js
+++ script/index.js
@@ -15,7 +15,7 @@ add_bt.addEventListener('click', function(){
     txt_bar.value = ''
     counter += 1
     check = document.querySelectorAll(' .checkbox')
-    console.log('Number of checkboxes:', check.length);
+    console.log('Number of checkboxes:', check.length)
     txt = document.querySelectorAll(' .set-text')
     }
 })
@@ -23,14 +23,14 @@ add_bt.addEventListener('click', function(){
     txt[i].classList.toggle('crossed')
 })*/
 tasks.addEventListener('click', function (event) {
-    const target = event.target;
-    const checkbox = target.closest('.checkbox');
+    const target = event.target
+    const checkbox = target.closest('.checkbox')
 
     if (checkbox) {
-        const index = Array.from(target.closest('.task').parentNode.children).indexOf(target.closest('.task'));
-        console.log('Checkbox clicked at index:', index);
+        const index = Array.from(target.closest('.task').parentNode.children).indexOf(target.closest('.task'))
+        console.log('Checkbox clicked at index:', index)
         // Now you can manipulate the corresponding set-text element
-        const txt = document.querySelectorAll('.set-text')[index];
+        const txt = document.querySelectorAll('.set-text')[index]
         txt.classList.toggle('crossed');
         if(checkbox.innerHTML == '<img src="assets and mages/91-918618_checked-checkbox-512-yellow-check-icon-orange-png.png" alt="">'){
         checkbox.innerHTML = '<img src="assets and mages/91-918618_checked-checkbox-512-yellow-check-icon-orange-png (1).png" alt="">'
@@ -39,17 +39,17 @@ tasks.addEventListener('click', function (event) {
             checkbox.innerHTML = '<img src="assets and mages/91-918618_checked-checkbox-512-yellow-check-icon-orange-png.png" alt="">'
         }
     }
-});
+})
 
 tasks.addEventListener('dblclick', function (event) {
     const target = event.target
-    const settxt = target.closest('.set-text');
+    const settxt = target.closest('.set-text')
 
     if (settxt) {
         const index = Array.from(target.closest('.task').parentNode.children).indexOf(target.closest('.task'))
         console.log('text choosed at index: ', index)
         // Now you can manipulate the corresponding set-text element
-        const text = document.querySelectorAll('.set-text')[index];
+        const text = document.querySelectorAll('.set-text')[index]
         //text.classList.toggle('crossed');
         let temp = text.innerText
         text.innerHTML = '<input name="edit-task" id="edit-task" placeholder="" />'
@@ -62,3 +62,14 @@ tasks.addEventListener('dblclick', function (event) {
         })
     }
 })
+
+tasks.addEventListener('click', function(event) {
+    const target = event.target
+    const del = target.closest('.del')
+    if(del){
+        const index = Array.from(target.closest('.task').parentNode.children).indexOf(target.closest('.task'))
+        const task = document.querySelectorAll('.task')[index]
+        tasks.removeChild(task)
+    }
+})
+
